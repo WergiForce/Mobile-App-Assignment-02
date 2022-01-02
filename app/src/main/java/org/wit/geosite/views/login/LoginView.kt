d267427db001c551df4b50694e48dc3ad46b0792
Ivan_de_Wergifosse <ivandewergifosse@hotmail.com>
2022-01-02 12:50:06 +0000

    Google Sign in implemented, currently minor error in writing to Realtime database

--- app/src/main/java/org/wit/geosite/views/login/LoginView.kt
+++ app/src/main/java/org/wit/geosite/views/login/LoginView.kt
@@ -1,14 +1,41 @@
 package org.wit.geosite.views.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import org.wit.geosite.R
 import org.wit.geosite.databinding.ActivityLoginBinding
+import org.wit.geosite.views.geositelist.GeositeListView
+import timber.log.Timber
 
 class LoginView : AppCompatActivity(){
+    private val RC_SIGN_IN: Int = 0
+    lateinit var googleSignInClient: GoogleSignInClient
     lateinit var presenter: LoginPresenter
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var loginIntentLauncher : ActivityResultLauncher<Intent>
+
+
+    //override fun onStart() {
+    //    super.onStart()
+    //    val currentUser = auth.currentUser
+    //    if(currentUser != null) {
+    //        val launcherIntent = Intent(this, GeositeListView::class.java)
+    //        loginIntentLauncher.launch(launcherIntent)
+    //       }
+    //}
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -40,7 +67,17 @@ class LoginView : AppCompatActivity(){
                 presenter.doLogin(email,password)
             }
         }
+
+        binding.googleSignIn.setSize(SignInButton.SIZE_WIDE)
+        binding.googleSignIn.setColorScheme(0)
+
+        createRequest()
+        
+        binding.googleSignIn.setOnClickListener{
+            signIn()
+        }    
     }
+
     fun showSnackBar(message: CharSequence){
         Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
             .show()
@@ -53,6 +90,39 @@ class LoginView : AppCompatActivity(){
     fun hideProgress() {
         binding.progressBar.visibility = View.GONE
     }
+
+    fun createRequest(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                presenter.firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                showSnackBar("Login Failed")
+            }
+        }
+    }
+
+
+
 
 
 }
